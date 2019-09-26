9fec52da1f4da1f4418f96f4a6f68dcd21cf50d5
ilkkav <ilkka.vanhanen@kapsi.fi>
2019-09-26 22:03:17 +0300

    Updates

---
+++ precache-manifest.98c953006f06512d5a48baa1a4d55ae1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f612567f822ed7116c7ceb5e0edddb",
+    "url": "/ilkkav/matikka/index.html"
+  },
+  {
+    "revision": "748b44f152e32fa40870",
+    "url": "/ilkkav/matikka/static/css/main.9cdf52ae.chunk.css"
+  },
+  {
+    "revision": "62f9195c233674ac8fac",
+    "url": "/ilkkav/matikka/static/js/2.f500ba0e.chunk.js"
+  },
+  {
+    "revision": "748b44f152e32fa40870",
+    "url": "/ilkkav/matikka/static/js/main.281a7edb.chunk.js"
+  },
+  {
+    "revision": "bc3df3e3729912b5bbf0",
+    "url": "/ilkkav/matikka/static/js/runtime~main.19d85368.js"
+  },
+  {
+    "revision": "c016e4676b6a153cfcbfb2884e25f62a",
+    "url": "/ilkkav/matikka/static/media/belle.c016e467.png"
+  },
+  {
+    "revision": "c7e8586cafe46316da1112b503bda301",
+    "url": "/ilkkav/matikka/static/media/carriage.c7e8586c.png"
+  }
+]);
