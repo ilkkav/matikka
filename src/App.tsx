bc02e55e4b75a6eb0df502def9f48ea93371a8cf
ilkkav <ilkka.vanhanen@kapsi.fi>
2019-10-10 17:19:59 +0300

    Pair images and words

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,17 @@
 import React, { useState } from 'react';
 import './App.css';
 import './styles.css';
-import { task, wordTask } from './task';
+import { task, getWordTasks, WordTask } from './task';
 import { TaskPage } from './TaskPage';
-import pirateShip from './pirate_ship.jpg';
-import princesses from './princesses.jpg';
-import butterfly from './butterfly.jpg'
-import carriage from './carriage.png';
 import { WordTaskPage } from './WordTaskPage';
 
-const getImage = (count: number) => count % 2 === 0 ? butterfly : princesses;
+const wordTasks: WordTask[] = getWordTasks()
 
 function App() {
 
   const handleTaskDone = (done: boolean) => {
-    setCurrentTaskDone(done);
     setDoneCount(doneCount + 1);
+    setCurrentTaskDone(done);
   }
 
   const [currentTaskDone, setCurrentTaskDone] = useState(false);
@@ -27,12 +23,12 @@ function App() {
     <div className="App">
       <div className="header flex-item alizarin">LYYTIN LASKUPELI</div>
       {currentTaskDone ?
-        <img className="flex-item color-2 responsive-image" src={getImage(doneCount)} onClick={() => {
+        <img className="flex-item color-2 responsive-image" src={wordTasks[doneCount - 1].image} onClick={() => {
           setCurrentTaskDone(false);
         }
         }></img>
         :
-        wordGameSelected ? <WordTaskPage input={wordTask()} setDone={handleTaskDone} />
+        wordGameSelected ? <WordTaskPage input={wordTasks[doneCount]} setDone={handleTaskDone} />
           : <TaskPage input={task()} setDone={handleTaskDone} />
       }
       <div className="flex-container narrow">
--- src/task.ts
+++ src/task.ts
@@ -1,3 +1,12 @@
+import butterfly from './butterfly.jpg'
+import pirateShip from './pirate_ship.jpg';
+import princesses from './princesses.jpg';
+import carriage from './carriage.png';
+import fireEngine from './fire_engine.jpg';
+import penguin from './penguin.jpg';
+import unicorn from './unicorn.jpg';
+import castle from './castle.jpg';
+import rainbow from './rainbow.jpg';
 
 export type Task = {
   first: number
@@ -8,6 +17,7 @@ export type Task = {
 export type WordTask = {
   letters: Letter[]
   blanks: string[]
+  image: any
 }
 
 type LetterStatus = 'Normal' | 'Hidden' | 'Solved'
@@ -17,6 +27,20 @@ export type Letter = {
   status: LetterStatus
 }
 
+const wordsAndImages: Record<string, any> = {
+  PERHONEN: butterfly,
+  PRINSESSA: princesses,
+  PALOAUTO: fireEngine,
+  SATEENKAARI: rainbow,
+  PINGVIINI: penguin,
+  LINNA: castle,
+  YKSISARVINEN: unicorn
+}
+
+export const getWordTasks = () => {
+  return Object.keys(wordsAndImages).map(key => wordTask(key))
+}
+
 export const task = (): Task=> {
   const result = Math.floor(Math.random() * 11); // 0...10
   const first = Math.floor(Math.random() * (result)); // < result
@@ -24,17 +48,9 @@ export const task = (): Task=> {
   return {first, second, result};
 }
 
-const words = [
-  'PERHONEN',
-  'PRINSESSA',
-  'PALOAUTO',
-  'SATEENKAARI'
-]
-
 const randomLessThan = (max: number) => Math.floor(Math.random()*max)
 
-export const wordTask = (): WordTask => {
-  const word = words[randomLessThan(words.length)]
+const wordTask = (word: string): WordTask => {
   const firstRandom = randomLessThan(word.length)
   let secondRandom
   while (true) {
@@ -46,5 +62,5 @@ export const wordTask = (): WordTask => {
   const blankIndices = [firstRandom, secondRandom].sort((a: number, b: number) => a-b)
   const letters: Letter[] = [...word].map(c => ({c, status: 'Normal'}))
   blankIndices.forEach(i => {letters[i].status = 'Hidden'})
-  return {letters, blanks: blankIndices.map(i => letters[i].c)}
+  return {letters, blanks: blankIndices.map(i => letters[i].c), image: wordsAndImages[word]}
 }
