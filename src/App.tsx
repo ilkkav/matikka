f82acacb7aa012661a8545f0f159555dbb108f9f
ilkkav <ilkka.vanhanen@kapsi.fi>
2019-10-13 09:33:35 +0300

    Fix done state for Nth rounds

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,36 @@ import { TaskPage } from './TaskPage';
 import { WordTaskPage } from './WordTaskPage';
 
 const wordTasks: WordTask[] = getWordTasks()
+const maxIndex = wordTasks.length;
 
 function App() {
 
-  const handleTaskDone = (done: boolean) => {
-    setDoneCount(doneCount + 1);
-    setCurrentTaskDone(done);
+  const nextTask = () => {
+    setDoneCount((doneCount + 1) % maxIndex);
   }
 
-  const [currentTaskDone, setCurrentTaskDone] = useState(false);
+  const [showPrize, setShowPrize] = useState(false);
   const [doneCount, setDoneCount] = useState(0);
   const [wordGameSelected, setWordGameSelected] = useState<boolean>(true);
 
   return (
     <div className="App">
       <div className="header flex-item alizarin">LYYTIN LASKUPELI</div>
-      {currentTaskDone ?
-        <img className="flex-item color-2 responsive-image" src={wordTasks[doneCount - 1].image} onClick={() => {
-          setCurrentTaskDone(false);
+      {showPrize ?
+        <img className="flex-item color-2 responsive-image" src={wordTasks[doneCount].image} onClick={() => {
+          setShowPrize(false);
+          nextTask();
         }
         }></img>
         :
-        wordGameSelected ? <WordTaskPage input={wordTasks[doneCount]} setDone={handleTaskDone} />
-          : <TaskPage input={task()} setDone={handleTaskDone} />
+        wordGameSelected ? <WordTaskPage input={wordTasks[doneCount]} setDone={() => setShowPrize(true)} />
+          : <TaskPage input={task()} setDone={() => setShowPrize(true)} />
       }
       <div className="flex-container narrow clouds">
         <button className="flex-item button small pomegranate hollow" value="valmis" onClick={() => {
           setWordGameSelected(!wordGameSelected)
-          setCurrentTaskDone(false)
+          setShowPrize(false);
+          nextTask();
         }} >
           VAIHDA PELI
         </button>
